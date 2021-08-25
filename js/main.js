eb6041718af0d6f3093b6429bc30705c4c49ce3a
Godson Oladipupo <godsonoladipupo6@gmail.com>
2021-08-25 08:31:06 +0100

    Ui design for the landing page of a gym website

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const navToggle = document.querySelector('.nav__toggle');
+const navMenu = document.querySelector('.nav__menu');
+const navLinks = document.querySelectorAll('.nav__link');
+
+let i = 1;
+navToggle.addEventListener('click', function(){
+    i++;
+    if (i % 2 === 0){
+        navToggle.classList.add('open');
+        navMenu.style.right = 0;
+    }else{
+        navToggle.classList.remove('open');
+        navMenu.style.right = '-100%';
+    };
+})
+
+navLinks.forEach(function(navLink){
+    navLink.addEventListener('click', function(){
+        navToggle.classList.remove('open');
+        navMenu.style.right = '-100%';
+    })
+})
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
+
+sr.reveal(`.main__content, .main__button, .headline, .welcome__message, .offers, .plans__message, .plans, .trainer__message,.testimonial__message, .bottom__logo, .testimony, .info, .footer__end`,{
+    origin: 'top',
+    interval: 150,
+})
+
+sr.reveal(`.trainer__img1, .trainer__img3`,{
+    origin: 'left',
+    interval: 150,
+})
+
+sr.reveal(`.trainer__img2, .trainer__img4`,{
+    origin: 'right',
+    interval: 150,
+})
